96cacb450e41840e1bdc0b0e60133cde4c727c9e
Fabián Vázquez <vazquezfabian123@gmail.com>
2024-06-01 04:09:34 -0600

    Más correcciones

--- frontend/src/views/App.test.tsx
+++ frontend/src/views/App.test.tsx
@@ -1,10 +1,11 @@
-import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+
 import App from '../App';
+import React from 'react';
 
 describe('App', () => {
   it('renders headline', () => {
-    render(<App {...{title: "React"}} />);
+    render(<App title="React" />);
 
     screen.debug();
 
